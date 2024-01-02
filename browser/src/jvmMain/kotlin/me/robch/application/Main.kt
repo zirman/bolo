b35287473c5e584aaec95ea5e48710e1123db883
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-01-02 17:20:55 -0500

    Added fibonacci speed tests

--- browser/src/jvmMain/kotlin/me/robch/application/Main.kt
+++ browser/src/jvmMain/kotlin/me/robch/application/Main.kt
@@ -5,6 +5,12 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
+//    val i = 38
+//
+//    measureTime {
+//        println("kotlin/wasm fib($i) ${fib(i)}")
+//    }.also { println("kotlin/wasm time: ${it.inWholeMilliseconds} ms") }
+
     embeddedServer(
         factory = Netty,
         port = 8080,
